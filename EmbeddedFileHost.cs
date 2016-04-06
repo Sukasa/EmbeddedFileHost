fc0ae53ec0928f3a2b6363eecadb52d5b45bd5d2
Sukasa <nick.m@shaw.ca>
2016-04-05 20:31:14 -0700

    Fixed bad assembly name and rewrite the whole thing to fix a bunch of bugs

--- EmbeddedFileHost.cs
+++ EmbeddedFileHost.cs
@@ -34,114 +34,125 @@ namespace SDBrowser
 
             while (true)
             {
-                if (UdpHost.Poll(-1, SelectMode.SelectRead))
+                try
                 {
-                    int PacketSize = UdpHost.ReceiveFrom(Buffer, ref Endpoint);
-
-                    string Packet = new string(Encoding.UTF8.GetChars(Buffer, 0, PacketSize));
-                    string Param = null;
-
-                    int Split = Packet.IndexOf(' ');
-                    if (Split > -1)
+                    if (UdpHost.Poll(-1, SelectMode.SelectRead))
                     {
-                        Param = Packet.Substring(Split);
-                        Packet = Packet.Substring(0, Split);
-                    }
+                        int PacketSize = UdpHost.ReceiveFrom(Buffer, ref Endpoint);
 
-                    byte[] Data;
-                    switch (Packet)
-                    {
-                        case "KNOCKKNOCK":
-                            Data = Encoding.UTF8.GetBytes("HIHI");
-                            UdpHost.SendTo(Data, Data.Length, SocketFlags.None, Endpoint);
+                        string Packet = new string(Encoding.UTF8.GetChars(Buffer, 0, PacketSize));
+                        string Param = null;
 
-                            // Send Status/etc
-                            Data = Encoding.UTF8.GetBytes("IAM " + FriendlyName);
-                            UdpHost.SendTo(Data, Data.Length, SocketFlags.None, Endpoint);
+                        int Split = Packet.IndexOf(' ');
+                        if (Split > -1)
+                        {
+                            Param = Packet.Substring(Split + 1);
+                            Packet = Packet.Substring(0, Split);
+                        }
 
-                            Data = Encoding.UTF8.GetBytes("CURRENT EmbeddedFileHost/1.0 " + Status);
-                            UdpHost.SendTo(Data, Data.Length, SocketFlags.None, Endpoint);
-
-                            Data = Encoding.UTF8.GetBytes("ROOT " + RootDir);
-                            UdpHost.SendTo(Data, Data.Length, SocketFlags.None, Endpoint);
-                            break;
-                        case "DIR":
-                            StringBuilder SB = new StringBuilder("LIST ");
-                            foreach (String Node in Directory.EnumerateFileSystemEntries(Param))
-                            {
-                                if (Directory.Exists(Node))
-                                {
-                                    SB.Append(Node);
-                                    SB.AppendLine("#DIR");
-                                }
-                                else if (File.Exists(Node))
-                                {
-                                    FileInfo F = new FileInfo(Node);
-
-                                    SB.Append(Node);
-                                    SB.Append("#");
-                                    SB.AppendLine(F.Length.ToString());
-                                }
-                            }
-                            Data = Encoding.UTF8.GetBytes(SB.ToString());
-                            UdpHost.SendTo(Data, Data.Length, SocketFlags.None, Endpoint);
-                            break;
-                        case "GET":
-                            {
-                                FileStream FS = File.OpenRead(Param);
-                                Data = Encoding.UTF8.GetBytes("READY " + FS.Length.ToString());
-                                UdpHost.SendTo(Data, Data.Length, SocketFlags.None, Endpoint);
-                                int Amt;
-
-                                while ((Amt = FS.Read(Buffer, 0, 512)) > 0)
-                                {
-                                    UdpHost.SendTo(Buffer, Amt, SocketFlags.None, Endpoint);
-                                }
-                                FS.Close();
-                                FS.Dispose();
-                                FS = null;
-                            }
-                            break;
-                        case "PUT":
-                            {
-                                String[] Params = Param.Split(' ');
-                                int AmtToGet = int.Parse(Params[1]);
-                                int AmtGotten = 0;
-                                if (File.Exists(Params[0]))
-                                    File.Delete(Params[0]);
-                                FileStream FS = File.OpenWrite(Params[0]);
-                                Data = Encoding.UTF8.GetBytes("READY " + Buffer.Length);
+                        byte[] Data;
+                        switch (Packet)
+                        {
+                            case "KNOCKKNOCK":
+                                Data = Encoding.UTF8.GetBytes("HIHI");
                                 UdpHost.SendTo(Data, Data.Length, SocketFlags.None, Endpoint);
 
-                                while (AmtGotten < AmtToGet)
+                                // Send Status/etc
+                                Data = Encoding.UTF8.GetBytes("IAM " + FriendlyName);
+                                UdpHost.SendTo(Data, Data.Length, SocketFlags.None, Endpoint);
+
+                                Data = Encoding.UTF8.GetBytes("CURRENT EmbeddedFileHost/1.0 " + Status);
+                                UdpHost.SendTo(Data, Data.Length, SocketFlags.None, Endpoint);
+
+                                Data = Encoding.UTF8.GetBytes("ROOT " + RootDir);
+                                UdpHost.SendTo(Data, Data.Length, SocketFlags.None, Endpoint);
+                                break;
+                            case "DIR":
+                                StringBuilder SB = new StringBuilder("LIST ");
+                                foreach (String Node in Directory.EnumerateFileSystemEntries(Param))
                                 {
-                                    int Amt = UdpHost.ReceiveFrom(Buffer, ref Endpoint);
-                                    FS.Write(Buffer, 0, Amt);
-                                    AmtGotten += Amt;
+                                    if (Directory.Exists(Node))
+                                    {
+                                        SB.Append(Node);
+                                        SB.AppendLine("#DIR");
+                                    }
+                                    else if (File.Exists(Node))
+                                    {
+                                        FileInfo F = new FileInfo(Node);
+
+                                        SB.Append(Node);
+                                        SB.Append("#");
+                                        SB.AppendLine(F.Length.ToString());
+                                    }
                                 }
-                                FS.Close();
-                                FS.Dispose();
-                                FS = null;
-                            }
-                            break;
-                        case "MKDIR":
-                            Directory.CreateDirectory(Param);
-                            Data = Encoding.UTF8.GetBytes("DONE");
-                            UdpHost.SendTo(Data, Data.Length, SocketFlags.None, Endpoint);
-                            break;
-                        case "DELETE":
-                            File.Delete(Param);
-                            Data = Encoding.UTF8.GetBytes("DONE");
-                            UdpHost.SendTo(Data, Data.Length, SocketFlags.None, Endpoint);
-                            break;
-                        case "DONE":
-                            break;
+                                Data = Encoding.UTF8.GetBytes(SB.ToString());
+                                UdpHost.SendTo(Data, Data.Length, SocketFlags.None, Endpoint);
+                                break;
+                            case "GET":
+                                {
+                                    FileStream FS = File.OpenRead(Param);
+                                    Data = Encoding.UTF8.GetBytes("READY " + FS.Length.ToString());
+                                    UdpHost.SendTo(Data, Data.Length, SocketFlags.None, Endpoint);
+                                    int Amt;
+
+                                    while ((Amt = FS.Read(Buffer, 0, 512)) > 0)
+                                    {
+                                        UdpHost.SendTo(Buffer, Amt, SocketFlags.None, Endpoint);
+                                    }
+                                    FS.Close();
+                                    FS.Dispose();
+                                    FS = null;
+                                }
+                                break;
+                            case "PUT":
+                                {
+                                    String[] Params = Param.Split(' ');
+                                    int AmtToGet = int.Parse(Params[1]);
+                                    int AmtGotten = 0;
+                                    if (File.Exists(Params[0]))
+                                        File.Delete(Params[0]);
+                                    FileStream FS = File.OpenWrite(Params[0]);
+                                    Data = Encoding.UTF8.GetBytes("READY " + Buffer.Length);
+                                    UdpHost.SendTo(Data, Data.Length, SocketFlags.None, Endpoint);
+
+                                    while (AmtGotten < AmtToGet)
+                                    {
+                                        int Amt = UdpHost.ReceiveFrom(Buffer, ref Endpoint);
+                                        FS.Write(Buffer, 0, Amt);
+                                        AmtGotten += Amt;
+                                    }
+                                    FS.Close();
+                                    FS.Dispose();
+                                    FS = null;
+                                    Data = Encoding.UTF8.GetBytes("DONE");
+                                    UdpHost.SendTo(Data, Data.Length, SocketFlags.None, Endpoint);
+                                }
+                                break;
+                            case "MKDIR":
+                                Directory.CreateDirectory(Param);
+                                Data = Encoding.UTF8.GetBytes("DONE");
+                                UdpHost.SendTo(Data, Data.Length, SocketFlags.None, Endpoint);
+                                break;
+                            case "DELETE":
+                                File.Delete(Param);
+                                Data = Encoding.UTF8.GetBytes("DONE");
+                                UdpHost.SendTo(Data, Data.Length, SocketFlags.None, Endpoint);
+                                break;
+                            case "DONE":
+                                break;
+                        }
+                        Data = null;
                     }
-                    Data = null;
+                    else
+                    {
+                        Thread.Sleep(10);
+                    }
                 }
-                else
+                catch (SocketException)
                 {
-                    Thread.Sleep(10);
+                    UdpHost.Close();
+                    Endpoint = new IPEndPoint(IPAddress.Any, 15000);
+                    UdpHost.Bind(Endpoint);
                 }
             }
 
